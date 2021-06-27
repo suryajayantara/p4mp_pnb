e497a267779ceae3b4ddb882d883e68077592a7b
Surya Jayantara <ptsuryajayantara01@gmail.com>
2021-06-27 18:21:39 +0800

    :sparkles: Initial Commit

---
+++ app/Models/Faculty.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Faculty extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'faculty_name',
+        'desc'
+    ];
+
+    public function departement(){
+        return $this->hasMany(Departement::class);
+    }
+
+}
